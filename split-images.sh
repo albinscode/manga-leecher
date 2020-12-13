6e9f6e9c27c62703cacb548e82d90b09d5790986
albinscode <albinscode@gmail.com>
2020-12-13 20:26:57 +0100

    split image with normal counter and repage

--- split-images.sh
+++ split-images.sh
@@ -14,12 +14,13 @@ OUTPUT='png'    # set extension of image type to save
 
 # You shouldn't need to adjust anything below here
 
-COUNTER=0       # set counter to zero
+COUNTER=1       # set counter to zero
 
 for ITEM in ${INPUT_FOLDER}/*.$INPUT        # get items in present folder
 
 do                          # start loop
 
+    echo ${ITEM}
     data=`identify $ITEM | awk '{print $3}'`                # get image data
     W=`echo $data | sed 's/[^0-9]/ /g' | awk '{print $1}'`  # extract width
     H=`echo $data | sed 's/[^0-9]/ /g' | awk '{print $2}'`  # extract height
@@ -27,21 +28,30 @@ do                          # start loop
     # Vertical part
     NEWW=$((W/2))                                           # set new width
 
-    LEFT=${OUTPUT_FOLDER}'/'${COUNTER}.0.$OUTPUT           # filename of left half
-    RIGHT=${OUTPUT_FOLDER}'/'${COUNTER}.1.$OUTPUT          # filename of right half
+    printf -v FORMAT "%03d" ${COUNTER}
+    LEFT=${OUTPUT_FOLDER}'/'${FORMAT}.$OUTPUT           # filename of left half
+    COUNTER=$((COUNTER+1))  # increment counter
+
+    printf -v FORMAT "%03d" ${COUNTER}
+    RIGHT=${OUTPUT_FOLDER}'/'${FORMAT}.$OUTPUT          # filename of right half
+
+    echo ${LEFT}
 
     convert -verbose -crop ${NEWW}x${H} $ITEM $LEFT         # make left half
-    convert -verbose -crop ${W}x${H}+${NEWW}-0 $ITEM $RIGHT # make right half
+    convert -verbose -crop ${W}x${H}+${NEWW}-0 +repage $ITEM $RIGHT # make right half
 
     # horizontal part
     # NEWH=$((H/2))                                           # set new height
 
-    # TOP=${OUTPUT_FOLDER}'/'${COUNTER}.0.$OUTPUT             # filename of top half
-    # BOTTOM=${OUTPUT_FOLDER}'/'${COUNTER}.1.$OUTPUT          # filename of bottom half
+    # printf -v FORMAT "%03d" ${COUNTER}
+    # TOP=${OUTPUT_FOLDER}'/'${FORMAT}.$OUTPUT             # filename of top half
+    # COUNTER=$((COUNTER+1))  # increment counter
+    # printf -v FORMAT "%03d" ${COUNTER}
+    # BOTTOM=${OUTPUT_FOLDER}'/'${FORMAT}.$OUTPUT          # filename of bottom half
 
     # convert -verbose -crop ${W}x${NEWH} $ITEM $TOP           # make top half
     # convert -verbose -crop ${W}x${H}+0+${NEWH} $ITEM $BOTTOM # make bottom half
 
     COUNTER=$((COUNTER+1))  # increment counter
-
 done                        # end loop
+
